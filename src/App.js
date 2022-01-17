a1af52f45557eeabb2da99f18ed18a37b51ac5e2
zzlhr <lhr5533@gmail.com>
2022-01-17 21:32:49 +0800

    webrtc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import RecordRTC from './RecordRTC'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [disable, setDisable] = useState(false)
+    const [video, setVideo] = useState()
+    let recorder;
+
+    function captureCamera(callback) {
+        navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function (camera) {
+            callback(camera);
+        }).catch(function (error) {
+            alert('Unable to capture your camera. Please check console logs.');
+            console.error(error);
+        });
+    }
+
+    function stopRecordingCallback() {
+        const _video = video
+        _video.src = _video.srcObject = null;
+        _video.muted = false;
+        _video.volume = 1;
+        _video.src = URL.createObjectURL(recorder.getBlob());
+        setVideo(_video)
+        recorder.camera.stop();
+        recorder.destroy();
+        recorder = null;
+    }
+
+    const btnStopRecordingClickHandler = () => {
+        setDisable(true);
+        recorder.stopRecording(stopRecordingCallback);
+    }
+    const btnStartRecordingClickHandler = () => {
+        setDisable(true);
+        captureCamera(function (camera) {
+            const _video = video
+            _video.muted = true;
+            _video.volume = 0;
+            _video.srcObject = camera;
+            setVideo(_video)
+            recorder = RecordRTC(camera, {
+                type: 'video'
+            });
+
+            recorder.startRecording();
+
+            // release camera on stopRecording
+            recorder.camera = camera;
+            setDisable(false)
+            // document.getElementById('btn-stop-recording').disabled = false;
+        });
+    }
+    useEffect({
+    }, [])
+    return (
+        <div className="App">
+            <button id="btn-start-recording" onClick={btnStartRecordingClickHandler}>Start Recording</button>
+            <button id="btn-stop-recording" onClick={btnStopRecordingClickHandler} disabled={disable}>Stop Recording
+            </button>
+            <video controls autoPlay playsInline/>
+            <video autoPlay/>
+        </div>
+    );
 }
 
 export default App;
